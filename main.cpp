4a244be768e75be663807fcec464832fd13dbfcc
Skooshie <james.mcenaney00@gmail.com>
2024-07-07 19:44:38 +0000

    CABLINK2LEDS

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     1000ms
-
+// Blinking rate in milliseconds for LED1 2Sec LED2 5Sec
+#define BLINKING_RATE1     2000ms
+#define BLINKING_RATE2     5000ms
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+    // Initialise the digital Ouputs for LED1 and LED 2
+    DigitalOut led1(LED1);
     DigitalOut led2(LED2);
     
 
-    while (true) {
-        led = !led;
-        led2 = !led2;
+     while (true) {
+        led1 = !led1;ThisThread::sleep_for(BLINKING_RATE1);
+        led2 = !led2;ThisThread::sleep_for(BLINKING_RATE2);
         
-        ThisThread::sleep_for(BLINKING_RATE);
+        
+        
+
     }
 }
